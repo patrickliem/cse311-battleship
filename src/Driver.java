207d8682fdf01e4ec6d8a028144aeb867607e9af
Patrick Liem <patrickliem00@gmail.com>
2020-02-23 14:48:08 -0500

    Added some more setup stuff

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,9 @@
 
 public class Driver {
-
+	
+	public static void main(String[] args) {
+		Model battleshipModel = new Model();
+		ViewText battleshipTextView = new ViewText();
+		ViewGUI battleshipGUIView = new ViewGUI();
+	}
 }
