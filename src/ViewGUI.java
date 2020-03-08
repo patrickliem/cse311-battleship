1bdd05c65a329b8abe37e9e064bfbb2d07f415af
Laura Jacob <laurajacob98@gmail.com>
2020-03-08 15:57:29 -0400

    Update src/ViewGUI.java
    created two grids of blue squares for opponent and player boards. Will test later

--- src/ViewGUI.java
+++ src/ViewGUI.java
@@ -1,8 +1,54 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.event.MouseListener;
 import java.util.Observable;
 
 public class ViewGUI extends View{
-
+	public JFrame frame;
+	
+	//messing around with graphics we shall see what happens here
+	private void paintBoards(Graphics g) {
+		//opponent grid 10 x 10 (no labels) filled blue for water
+		 GridLayout oppGrid = new GridLayout(10, 10, 0, 0);
+		 frame.setLayout(oppGrid);
+		 for (int col = 0; col < 10; col++) {
+             for (int row = 0; row < 10; row++) {
+            	 g.setColor(Color.blue);
+                 g.fillRect(col, row, 50, 50);
+            }
+         }
+		//player grid 10 x 10 (no labels) filled blue for water
+		 GridLayout playerGrid = new GridLayout(10, 10, 0, 0);
+		 frame.setLayout(playerGrid);
+		 for (int col = 0; col < 10; col++) {
+             for (int row = 0; row < 10; row++) {
+            	 g.setColor(Color.blue);
+                 g.fillRect(col, row, 50, 50);
+            }
+         }
+	}
+	
+	
+	public ViewGUI() {
+		frame = new JFrame("Battleship (GUI Version)");
+		//size same as ViewText for now
+		frame.setSize(700,700);
+		frame.getContentPane().add(frame, new ViewGUI());
+		frame.setLocationRelativeTo(null);
+		frame.setBackground(Color.LIGHT_GRAY);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		
+		 
+	}
+	
+	
 	@Override
 	public void displayError(String errText) {
 		// TODO Auto-generated method stub
