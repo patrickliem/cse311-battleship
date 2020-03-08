562a61bddad77f517510c113a43e0e92c3b9e746
Laura Jacob <laurajacob98@gmail.com>
2020-03-08 14:11:41 -0400

    Update src/ViewGUI.java
    ViewGUI extends View

--- src/ViewGUI.java
+++ src/ViewGUI.java
@@ -1,4 +1,36 @@
+import java.awt.event.MouseListener;
+import java.util.Observable;
 
-public class ViewGUI {
+public class ViewGUI extends View{
+
+	@Override
+	public void displayError(String errText) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clearError() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayConfirmation(String confText) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addController(MouseListener controller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
